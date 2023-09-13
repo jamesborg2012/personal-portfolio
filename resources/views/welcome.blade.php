589fb7e51d88f8d0b400b14ac105a5df34f5822a
jamesborg2012 <jamesborg2012@gmail.com>
2023-09-13 21:24:48 +0200

    Made changes to load example Vue component
    Signed-off-by: jamesborg2012 <jamesborg2012@gmail.com>

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,12 +16,17 @@
         </style>
 
         @vite('resources/css/app.css')
+        @vite('resources/js/app.js')
     </head>
     <body class="antialiased">
         <div class="tailwind">
             <h1 class="text-3xl font-bold underline">
                 Hello World!
             </h1>
+        </div>
+
+        <div id="app">
+            <example-component></example-component>
         </div>
 
         <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
