0bc1dc08d2dff58a06c0a00a17c8fb59f99406c2
jamesborg2012 <jamesborg2012@gmail.com>
2023-09-17 19:44:40 +0200

    Added MenuLinks module to Twill
    Signed-off-by: jamesborg2012 <jamesborg2012@gmail.com>

---
+++ app/Models/MenuLink.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use A17\Twill\Models\Behaviors\HasPosition;
+use A17\Twill\Models\Behaviors\HasNesting;
+use A17\Twill\Models\Behaviors\Sortable;
+use A17\Twill\Models\Model;
+
+class MenuLink extends Model implements Sortable
+{
+    use HasPosition, HasNesting;
+
+    protected $fillable = [
+        'published',
+        'title',
+        'description',
+        'position',
+    ];
+    
+}
