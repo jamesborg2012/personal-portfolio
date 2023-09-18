5bd7a37a67500676448557cf9ea74e1861dcfcba
jamesborg2012 <jamesborg2012@gmail.com>
2023-09-18 21:15:02 +0200

    Added ability for Skills modules to be ordered
    Signed-off-by: jamesborg2012 <jamesborg2012@gmail.com>

--- app/Http/Controllers/Twill/SkillController.php
+++ app/Http/Controllers/Twill/SkillController.php
@@ -18,6 +18,7 @@ class SkillController extends BaseModuleController
     protected function setUpController(): void
     {
         $this->disablePermalink();
+        $this->enableReorder();
     }
 
     /**
