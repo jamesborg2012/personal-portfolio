b9c926c5713860d027211f6a101ad3909c6d68ca
jamesborg2012 <jamesborg2012@gmail.com>
2023-09-24 18:09:41 +0200

    Configured and setup the page footers
    Signed-off-by: jamesborg2012 <jamesborg2012@gmail.com>

---
+++ resources/views/components/left-footer.blade.php
@@ -0,0 +1,6 @@
+<div class='relative left-footer'>
+    <span class='absolute bottom-0 copyright'>{{ $copyright_name }} © @if ($show_year)
+        {{ $current_year }}
+    @endif
+    </span>
+</div>
---
+++ resources/views/components/right-footer.blade.php
@@ -0,0 +1,13 @@
+<div class='text-right right-footer'>
+    <ul>
+        @if (!empty($link_data))
+            @foreach ($link_data as $single_link)
+                <li class='inline mr-5'>
+                    <a href='{{ $single_link['url'] }}' title='{{ $single_link['title'] }}' target='_blank'>
+                        <i class="text-4xl {{ $single_link['icon'] }}"></i>
+                    </a>
+                </li>
+            @endforeach
+        @endif
+    </ul>
+</div>
